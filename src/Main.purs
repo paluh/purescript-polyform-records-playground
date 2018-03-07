3c9c8160c5af9c463d22f77c45e60083cca819ed
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-03-07 15:23:00 -0800

    Updated with type signatures.

--- src/Main.purs
+++ src/Main.purs
@@ -3,11 +3,11 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Random (random)
+import Control.Monad.Eff.Random (random, RANDOM)
 import Data.Array (any, elem)
-import Data.Maybe (Maybe(..))
-import Data.String (Pattern(..), contains, indexOf, length, toCharArray)
+import Data.String (Pattern(Pattern), contains, length, toCharArray)
 import Data.Tuple (Tuple(..))
 import Data.Variant (Variant, inj)
 import Debug.Trace (traceAnyA)
@@ -24,49 +24,73 @@ type Input err value = V (Array err) value
 
 -- | ...of course they are really dummy validators ;-)
 
+emailFormat :: ∀ m err
+  . Monad m
+  => Validation m (Array (Variant (emailFormat :: String | err))) String String
 emailFormat = Validation.hoistFnV \e →
   if contains (Pattern "@") e
     then pure e
     else Invalid [inj (SProxy ∷ SProxy "emailFormat") e]
 
-emailIsUsed = Validation.hoistFnMV \e → do
+emailIsUsed :: ∀ m eff err
+  . MonadEff (random :: RANDOM | eff) m
+ => Validation m (Array (Variant (isUsed :: String | err))) String String
+emailIsUsed = Validation.hoistFnMV \email -> do
   -- | Some effectful computation inside your monad.
   -- | Let's toss a coin instead of checking db
   -- | if email is really used>
-  v ← random
+  v ← liftEff random
   pure $ if v > 0.5
-    then Invalid [inj (SProxy ∷ SProxy "isUsed") e]
-    else pure e
+    then Invalid [ inj (SProxy ∷ SProxy "isUsed") email ]
+    else pure email
 
+emailFieldValidation :: ∀ m eff err
+  . MonadEff (random :: RANDOM | eff) m
+ => Validation m (Array (Variant (emailFormat :: String, isUsed :: String | err))) String String
 emailFieldValidation = emailFormat *> emailIsUsed
 
 -- | Let's define some simple validators for password field.
 
+minLength :: ∀ m err
+  . Monad m
+ => Int
+ -> Validation m (Array (Variant (minLength :: Tuple Int String | err))) String String
 minLength m = Validation.hoistFnV \p →
   if length p < m
     then Invalid [inj (SProxy ∷ SProxy "minLength") (Tuple m p)]
     else pure p
 
+maxLength :: ∀ m err
+  . Monad m
+ => Int
+ -> Validation m (Array (Variant (maxLength :: Tuple Int String | err))) String String
 maxLength m = Validation.hoistFnV \p →
   if length p > m
-    then Invalid [inj (SProxy ∷ SProxy "maxLength") (Tuple m p)]
+    then Invalid [ inj (SProxy ∷ SProxy "maxLength") (Tuple m p) ]
     else pure p
 
+hasDigit :: ∀ m err
+  . Monad m
+ => Validation m (Array (Variant (hasDigit :: String | err))) String String
 hasDigit = Validation.hoistFnV \p →
   let
     chars = toCharArray p
   in
     if any (_ `elem` chars) (toCharArray "0123456789")
       then pure p
-      else Invalid [inj (SProxy ∷ SProxy "hasDigit") p]
+      else Invalid [ inj (SProxy ∷ SProxy "hasDigit") p ]
 
-passwordFieldValidation min max = maxLength max *> minLength min *> hasDigit
+passwordFieldValidation :: ∀ m err
+  . Monad m
+ => Int
+ -> Int
+ -> Validation m (Array (Variant (hasDigit :: String, maxLength :: Tuple Int String, minLength :: Tuple Int String | err))) String String
+passwordFieldValidation min max =
+  maxLength max *> minLength min *> hasDigit
 
 data Field
   = EmailField (Input (Variant (emailFormat ∷ String, isUsed ∷ String)) String)
   | PasswordField (Input (Variant (hasDigit ∷ String, maxLength ∷ Tuple Int String, minLength ∷ Tuple Int String)) String)
-
-
 
 -- | Form types and form related helpers and validations
 
@@ -83,10 +107,16 @@ type Form = Tuple (Array String) (Array Field)
 -- | Here we can also observe that validation is
 -- | nothing more than function from input to V
 -- | in monadic context.
-fieldForm fetchValue constructor fieldValidation =
+formFromField :: forall m record input output err
+  . Monad m
+ => (record -> input)
+ -> (V err output -> Field)
+ -> Validation m err input output
+ -> Validation m Form record output
+formFromField getField constructor fieldValidation =
   Validation $ \inputRecord → do
     -- | Fetch field value from record using fetcher
-    let inputValue = fetchValue inputRecord
+    let inputValue = getField inputRecord
     -- | Run field validation agains this value
     r ← Validation.runValidation fieldValidation inputValue
     -- | Based on field validation result let's return:
@@ -98,10 +128,16 @@ fieldForm fetchValue constructor fieldValidation =
       -- | can be combined with other forms
       Invalid e → Invalid (Tuple [] [constructor (Invalid e)])
 
-emailForm = fieldForm (_.email) EmailField emailFieldValidation
+emailForm :: ∀ r m eff. MonadEff (random :: RANDOM | eff) m => Validation m Form { email :: String | r } String
+emailForm = formFromField _.email EmailField emailFieldValidation
 
-buildPasswordForm fetch = fieldForm fetch PasswordField (passwordFieldValidation 5 50)
+buildPasswordForm :: ∀ r m
+  . Monad m
+ => (r -> String)
+ -> Validation m Form r String
+buildPasswordForm getField = formFromField getField PasswordField (passwordFieldValidation 5 50)
 
+passwordForm :: ∀ m r. Monad m => Validation m Form { password1 :: String, password2 :: String | r } String
 passwordForm
   = ({password1: _, password2: _} <$> (buildPasswordForm _.password1) <*> (buildPasswordForm _.password2))
   -- | Here we are composing validations
@@ -118,8 +154,12 @@ passwordForm
       then Invalid (Tuple ["Password dont match"] [])
       else pure password1
 
+signupForm :: ∀ r m eff
+  . MonadEff (random :: RANDOM | eff) m
+ => Validation m Form { password1 :: String, password2 :: String, email :: String | r } { password :: String, email :: String }
 signupForm = {password: _, email: _} <$> passwordForm <*> emailForm
 
+printResult :: ∀ err eff a. V err a -> Eff (console :: CONSOLE | eff) Unit
 printResult =
   case _ of
     Valid form value → do
@@ -132,18 +172,19 @@ printResult =
       log "FORM INVALID:"
       traceAnyA form
 
+main :: ∀ eff. Eff (console :: CONSOLE, random :: RANDOM | eff) Unit
 main = do
   log "EXAMPLE"
 
-  v1 ← runValidation signupForm {email: "wrongemailformat", password1: "shrt", password2: "nodigits"}
+  v1 <- runValidation signupForm {email: "wrongemailformat", password1: "shrt", password2: "nodigits"}
   printResult v1
 
   log "\n\n"
 
-  v2 ← runValidation signupForm {email: "email@example.com", password1: "password1", password2: "password2"}
+  v2 <- runValidation signupForm {email: "email@example.com", password1: "password1", password2: "password2"}
   printResult v2
 
   log "\n\n"
 
-  v3 ← runValidation signupForm {email: "email@example.com", password1: "password921", password2: "password921"}
+  v3 <- runValidation signupForm {email: "email@example.com", password1: "password921", password2: "password921"}
   printResult v3
