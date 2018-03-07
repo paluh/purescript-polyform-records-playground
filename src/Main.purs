1a32cb8616bb6a5c5fe78b4e0531500dfd7683ac
Tomasz Rybarczyk <paluho@gmail.com>
2018-03-07 23:08:32 +0100

    Cosmetics

--- src/Main.purs
+++ src/Main.purs
@@ -16,7 +16,8 @@ import Polyform.Validation as Validation
 import Type.Prelude (SProxy(..))
 
 -- | Let's assume that our fields are really simple
--- | and contain only validation result
+-- | and contain only validation result.
+-- | Errors are kept in `Array`
 type Input err value = V (Array err) value
 
 -- | Let's define some simple validators for email field
@@ -70,14 +71,27 @@ data Field
 type Form = Tuple (Array String) (Array Field)
 
 -- | Let's build our form without any external helpers
--- | This function builds single field form
--- | given field validation.
+-- | This function builds single field form from:
+-- |  * value fetcher
+-- |  * field type constructor
+-- |  * field validation
+-- |
+-- | Here we can also observe that validation is
+-- | nothing more then function from input to V
+-- | in monadic context.
 fieldForm fetchValue constructor fieldValidation =
   Validation $ \inputRecord → do
+    -- | Fetch field value from record using fetcher
     let inputValue = fetchValue inputRecord
+    -- | Run field validation agains this value
     r ← Validation.runValidation fieldValidation inputValue
+    -- | Based on field validation result let's return:
     pure $ case r of
+      -- | form togheter with result value
+      -- | so we can combine both into larger values and forms
       Valid e v → Valid (Tuple [] [constructor (Valid e v)]) v
+      -- | or form as representation of our error which
+      -- | can be combined with other forms
       Invalid e → Invalid (Tuple [] [constructor (Invalid e)])
 
 emailForm = fieldForm (_.email) EmailField emailFieldValidation
@@ -86,7 +100,6 @@ buildPasswordForm fetch = fieldForm fetch PasswordField (passwordFieldValidation
 
 passwordForm
   = ({password1: _, password2: _} <$> (buildPasswordForm _.password1) <*> (buildPasswordForm _.password2))
-  -- | we are operating here on form component level
   >>> Validation.hoistFnV \{ password1, password2 } →
     if password1 /= password2
       then Invalid (Tuple ["Password dont match"] [])
